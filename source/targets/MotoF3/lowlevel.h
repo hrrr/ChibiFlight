578c10fbe04073ee27f8cc287d41de1dcb1d499f
hrrr <Pablo@luft.speedport_w_724v_09011603_00_023>
2016-03-27 16:45:11 +0200

    License

--- source/targets/MotoF3/lowlevel.h
+++ source/targets/MotoF3/lowlevel.h
@@ -3,6 +3,21 @@
  *
  *  Created on: Jan 17, 2016
  *      Author: Pablo
+ *
+ *  This file is part of ChibiFlight.
+
+    ChibiFlight is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ChibiFlight is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ChibiFlight.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef SOURCE_TARGETS_MOTOF3_LOWLEVEL_H_
